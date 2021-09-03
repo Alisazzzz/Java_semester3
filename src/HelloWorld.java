d4af3cdbc16d374ff1cd9af01b6d457b50e656f8
Alisa <alisa@mikryukov.info>
2021-09-03 14:08:11 +0300

    changes in HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         //comma is added
+        //new string of code
+        System.out.println("My name is Alisa.");
     }
 }
